7e41844fa6445b808aec63054d2a35ead7a18879
Nicolo <nicoloromito@protonmail.com>
2025-01-25 23:21:24 +0100

    [UPDATE]: Add NFTManager, ProofOfDegree, ProofOfWork contracts.

---
+++ hackathon/packages/hardhat/contracts/ProofOfWork.sol
@@ -0,0 +1,83 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+// Useful for debugging. Remove when deploying to a live network.
+import "hardhat/console.sol";
+
+// Use openzeppelin to inherit battle-tested implementations (ERC20, ERC721, etc)
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * A smart contract that allows changing a state variable of the contract and tracking the changes
+ * It also allows the owner to withdraw the Ether in the contract
+ * @author BuidlGuidl
+ */
+
+contract ProofOfWork is ERC721, Ownable {
+    // * state variables
+    uint256 private	_tokenId; // the token id
+	uint256	private	_dateFrom;
+	uint256	private	_dateTo;
+    string  private	_description;
+    string  private	_role;
+    string  private	_companyName;
+
+    constructor(address initialOwner) ERC721("ProofOfWork", "PoW") Ownable(initialOwner) {
+        _tokenId = 0;
+    }
+
+    function mint(address to, uint256 id) public {
+        require(id > 0, "Token ID must be greater than 0");
+        _tokenId = id;
+        _safeMint(to, _tokenId);
+    }
+
+    // * SETTERS //
+
+    function setDescription(string memory description) public {
+        _description = description;
+    }
+
+    function setCompanyName(string memory companyName) public {
+        _companyName = companyName;
+    }
+
+    function setRole(string memory role) public {
+        _role = role;
+    }
+
+    function setDateFrom(uint256 dateFrom) public {
+        _dateFrom = dateFrom;
+    }
+
+    function setDateTo(uint256 dateTo) public {
+        _dateTo = dateTo;
+    }
+
+    // * GETTERS //
+
+    function getTokenId() public view returns (uint256) {
+        return _tokenId;
+    }
+
+    function getDescription() public view returns (string memory) {
+        return _description;
+    }
+
+    function getCompanyName() public view returns (string memory) {
+        return _companyName;
+    }
+
+    function getRole() public view returns (string memory) {
+        return _role;
+    }
+
+    function getDateFrom() public view returns (uint256) {
+        return _dateFrom;
+    }
+
+    function getDateTo() public view returns (uint256) {
+        return _dateTo;
+    }
+}
