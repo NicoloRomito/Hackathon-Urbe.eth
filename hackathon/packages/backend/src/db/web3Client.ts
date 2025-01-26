5fd0d5f63b134071613c631d4c103c822b757448
Leone Riccardo <riccardo.leone.lavoro@gmail.com>
2025-01-26 09:46:44 +0100

    [set up variable env]

--- hackathon/packages/backend/src/db/web3Client.ts
+++ hackathon/packages/backend/src/db/web3Client.ts
@@ -4,7 +4,7 @@ import managerAbi from '../models/abi/smartContractManagerABI';
 
 dotenv.config();
 
-const web3 = new Web3(new Web3.providers.WebsocketProvider("ws://localhost:8545")); // Adjust to your local chain's WebSocket port
+const web3 = new Web3(new Web3.providers.WebsocketProvider("wss://ethereum-sepolia-rpc.publicnode.com")); // Adjust to your local chain's WebSocket port
 
 // Instantiate contract
 console.log("Connecting to contract manager at", process.env.CONTRACT_MANAGER);
